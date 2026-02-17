42bd260395e6436fb5354cef7faef8210c143b39
Nikhilreddy1 <nikhilkr.nik@gmail.com>
2026-02-17 17:27:26 +0530

    Added model trainer file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,6 +11,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path:str = os.path.join('artifact','train.csv')
@@ -52,4 +55,7 @@ if __name__ == "__main__":
     train_data, test_Data = obj.initiate_data_ingestion()
     
     data_transformation = DataTransformation()
-    data_transformation.initiate_Data_transformation(train_data, test_Data)
+    train_arr, test_arr,_ = data_transformation.initiate_Data_transformation(train_data, test_Data)
+    
+    ModelTrainer = ModelTrainer()
+    print(ModelTrainer.initiate_model_trainer(train_arr, test_arr))
---
+++ src/components/model_trainer.py
@@ -0,0 +1,82 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from sklearn.ensemble import(
+    RandomForestClassifier,
+    GradientBoostingClassifier,
+    AdaBoostClassifier
+)
+
+from catboost import CatBoostClassifier
+
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from xgboost import XGBRegressor
+
+from sklearn.metrics import r2_score
+
+from src.exception import CustomException
+from src.logger import logging  
+
+from src.utils import save_object, evaluate_model
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join('artifacts', 'model.pkl')
+    
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+        
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            logging.info("Splitting training and test input data")
+            X_train,y_train,X_test,y_test = (
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+            
+            models = {
+                "Random Forest": RandomForestClassifier(),
+                "Gradient Boosting": GradientBoostingClassifier(),
+                "AdaBoost": AdaBoostClassifier(),
+                "CatBoost": CatBoostClassifier(verbose=False),
+                "XGBRegressor": XGBRegressor(),
+                "Linear Regression": LinearRegression(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "KNN": KNeighborsRegressor()
+            }
+            
+            model_report:dict = evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, 
+                                                models=models)
+            
+            ## To get the best model score from dict
+            best_model_score = max(sorted(model_report.values()))
+            
+            ## To get the best model name from dict
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            
+            best_model = models[best_model_name]
+            
+            if best_model_score < 0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best found model on both training and testing dataset is {best_model_name} with r2 score: {best_model_score}")
+            
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+            
+            predicted = best_model.predict(X_test)
+            r2_square = r2_score(y_test, predicted)
+            logging.info(f"R2 score of best model is {r2_square}")
+                
+        except Exception as e:
+            raise CustomException(e, sys)
+
